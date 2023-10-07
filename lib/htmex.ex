c9b2dac4b790f2c8dfbba2b9f1cbd317b38f1196
Rosa Richter <cosmic.lady.rosa@gmail.com>
2023-10-07 03:09:42 +0000

    New Mix project

---
+++ lib/htmex.ex
@@ -0,0 +1,18 @@
+defmodule Htmex do
+  @moduledoc """
+  Documentation for `Htmex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Htmex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/htmex_test.exs
@@ -0,0 +1,8 @@
+defmodule HtmexTest do
+  use ExUnit.Case
+  doctest Htmex
+
+  test "greets the world" do
+    assert Htmex.hello() == :world
+  end
+end
